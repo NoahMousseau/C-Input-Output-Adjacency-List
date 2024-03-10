b90e714da5c29a74195a32298f3ff300f7ec1db7
Noah Mousseau <112453632+NoahMousseau@users.noreply.github.com>
2024-03-10 17:40:09 -0400

    Adding bubbleSort in descending order to program

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -53,6 +53,44 @@ public:
         head = sorted;
     }
 
+    //Function to use bubble sort with linked lists in descending order
+    void bubbleSort() {
+        if (head == nullptr || head->next == nullptr) {
+            //If the list is empty or has only one element, it is already sorted
+            return;
+        }
+
+        bool swapped;
+        Node* current;
+        Node* lastSorted = nullptr;
+
+        do {
+            swapped = false;
+            current = head;
+
+            while (current->next != lastSorted) {
+                if (current->data < current->next->data) {
+                    //Swap nodes if they are in the wrong order
+                    int tempData = current->data;
+                    char tempDataK1 = current->dataK1;
+                    char tempDataK2 = current->dataK2;
+
+                    current->data = current->next->data;
+                    current->dataK1 = current->next->dataK1;
+                    current->dataK2 = current->next->dataK2;
+
+                    current->next->data = tempData;
+                    current->next->dataK1 = tempDataK1;
+                    current->next->dataK2 = tempDataK2;
+
+                    swapped = true;
+                }
+                current = current->next;
+            }
+            lastSorted = current;
+        } while (swapped);
+    }
+
     //Function to insert a new node at the end of the list
     void insertAtEnd(int intValue, char charValueK1, char charValueK2) {
         Node* newNode = new Node(intValue, charValueK1, charValueK2);
--- Main.cpp
+++ Main.cpp
@@ -17,6 +17,7 @@ int main() {
     cout << "Choose method of sorting:" << endl;
     cout << "Primary operation of inserting nodes into corresponding Linked Lists: 1" << endl;
     cout << "Using insertionSort to sort Linked Lists: 2" << endl;
+    cout << "Using bubbleSort to sort Linked Lists: 3" << endl;
     cin >> userChoice;
     cout << endl;
 
@@ -160,6 +161,84 @@ int main() {
         for (LinkedList& ll : Vertices) {
             Node* head = ll.getHead();
             ll.insertionSort(); //Call insertionSort
+        }
+
+        //Display linked lists
+        int size = Vertices.size();
+        for (int i = 0; i < size; i++) {
+            cout << Vertices[i].getKey();
+            Vertices[i].display();
+        }
+
+        //Close the file
+        inputFile.close();
+    } else if (userChoice == 3) {
+        //Read input from the file
+        ifstream inputFile("input.txt");
+
+        //Created required array to hold linked lists
+        vector<LinkedList> Vertices;
+
+        //Get first line of file and create linked lists
+        string firstLine;
+        getline(inputFile, firstLine);
+
+        // Create linked lists for each character in the first line of the file
+        for (char ch : firstLine) {
+            if (ch != ' ') {
+                LinkedList newList;
+                newList.setKey(ch);
+                Vertices.push_back(newList);
+            }
+        }
+
+        //Set variables for information from text file
+        char source;
+        char destination;
+        int weight;
+
+        //Place file contents into corresponding linked lists
+        string line;
+        while (getline(inputFile, line)) {
+            int commaCount = 0;
+            int spaceCount = 0;
+            string strSave;
+            for (char ch : line) {
+                if (ch != ',' && ch != ' ') {
+                    //Compare to find which token is being looked at
+                    if (commaCount == 0 && spaceCount == 0) {
+                        source = ch;
+                    }
+                    else if (commaCount == 1 && spaceCount == 1) {
+                        destination = ch;
+                    }
+                    //Combine last 2 chars of each line to form an int
+                    else if (commaCount == 2 && spaceCount == 2) {
+                        strSave = strSave + ch;
+                        //Convert string to into to save to weight
+                        weight = stoi(strSave);
+                    }
+                }
+                //Determine if ch being looked at is ',' or ' '
+                else if (ch == ',') {
+                    commaCount++;
+                }
+                else if (ch == ' ') {
+                    spaceCount++;
+                }
+            }
+            //Iterate through linked lists and place weight-source-destination nodes accordingly
+            for (LinkedList& ll : Vertices) {
+                if (ll.getKey() == source) {
+                    ll.insertAtEnd(weight, source, destination);
+                }
+            }
+        }
+
+        //Sort linked lists
+        for (LinkedList& ll : Vertices) {
+            Node* head = ll.getHead();
+            ll.bubbleSort(); //Call bubbleSort
         }
 
         //Display linked lists
